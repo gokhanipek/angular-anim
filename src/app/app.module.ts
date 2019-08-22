e31fe14c2eaf5ecc51b45d204c13b0f40961ab59
unknown <gokhan.ipek@etiya.com>
2019-08-23 01:45:09 +0300

    'sun-and-beach'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AnimationsComponent } from './animations/animations.component';
+import { DivingComponent } from './diving/diving.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AnimationsComponent
+    AnimationsComponent,
+    DivingComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/diving/diving.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-diving',
+    templateUrl: './diving.component.html',
+    styleUrls: ['./diving.component.css']
+})
+export class DivingComponent {
+    constructor() { }
+
+}
